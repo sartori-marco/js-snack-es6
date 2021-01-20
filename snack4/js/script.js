614918b62756ea39a233d20a361169bbc8936085
marco sartori <marcosartori98@yahoo.com>
2021-01-20 14:12:43 +0100

    start snack 4

---
+++ snack4/js/script.js
@@ -0,0 +1,35 @@
+// jsnack 1
+// Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal.
+
+// creo l'array di oggetti
+
+const bici = [
+  {
+    nome: 'bici1',
+    peso: 10
+  },
+
+  {
+    nome: 'bici2',
+    peso: 6
+  },
+
+  {
+    nome: 'bici3',
+    peso: 15
+  },
+
+  {
+    nome: 'bici4',
+    peso: 2
+  }
+];
+
+// uso destructuring per determinare quale pesa meno
+
+
+
+
+
+//
